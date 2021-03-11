903b8523ae9164227171f8b8bd1cad35173f0487
jekornhauser <jekornhauser@gmail.com>
2021-03-10 22:00:06 -0500

    pushed changes

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ def index():
     '''Return the homepage.'''
     return render_template('index.html')
 
-@app.route('/bankcount/<country>')
-def bankcount(country):
-    '''Returns number of banks in each country at each period.'''
+@app.route('/bankcountrycount/<country>')
+def bankcountrycount(country):
+    '''Returns total number of banks over time in the selected country.'''
     x = []
     y = []
     with engine.connect() as con:
@@ -33,6 +33,68 @@ def bankcount(country):
             y.append(row[0])
             x.append(row[1])
     return jsonify(x,y)
+
+@app.route('/banktimecount/<timeperiod>')
+def banktimecount(timeperiod):
+    '''Returns number of banks in each country at selected time period.'''
+    x = []
+    y = []
+    with engine.connect() as con:
+        rs = con.execute('SELECT COUNT("Bank"), "Country" FROM banks WHERE "Balance Sheet Date"=? GROUP BY "Country"',timeperiod)
+        for row in rs:
+            y.append(row[0])
+            x.append(row[1])
+    return jsonify(x,y)
+
+@app.route('/tacountrysum/<country>')
+def tacountrysum(country):
+    '''Returns sum of total assets  in billions USD over time in the selected country.'''
+    x = []
+    y = []
+    with engine.connect() as con:
+        rs = con.execute('SELECT SUM("Total Assets"), "Balance Sheet Date" FROM banks WHERE "Country"=? GROUP BY "Balance Sheet Date"',country)
+        for row in rs:
+            y.append(row[0])
+            x.append(row[1])
+    return jsonify(x,y)
+
+@app.route('/tatimesum/<timeperiod>')
+def tatimesum(timeperiod):
+    '''Returns sum of total assets  in billions USD in all countries given the selected time period.'''
+    x = []
+    y = []
+    with engine.connect() as con:
+        rs = con.execute('SELECT SUM("Total Assets"), "Country" FROM banks WHERE "Balance Sheet Date"=? GROUP BY "Country"',timeperiod)
+        for row in rs:
+            y.append(row[0])
+            x.append(row[1])
+    return jsonify(x,y)
+
+@app.route('/bankassets/<bank>')
+def bankassets(bank):
+    '''Returns the change in assets over time at the selected bank.'''
+    x = []
+    y = []
+    with engine.connect() as con:
+        rs = con.execute('SELECT "Total Assets", "Balance Sheet Date" FROM banks WHERE "Bank"=?',bank)
+        for row in rs:
+            y.append(row[0])
+            x.append(row[1])
+    return jsonify(x,y)
+
+@app.route('/tables/<timeperiod>')
+def tables(timeperiod):
+    '''Returns the details of the ten largest banks based on total assets at the selected time period.'''
+    bank_name = []
+    country = []
+    assets = []
+    with engine.connect() as con:
+        rs = con.execute('SELECT "Bank", "Country", "Total Assets" FROM banks WHERE "Balance Sheet Date"=? ORDER BY "Total Assets" DESC LIMIT 10',timeperiod)
+        for row in rs:
+            bank_name.append(row[0])
+            country.append(row[1])
+            assets.append(row[2])
+    return jsonify(bank_name,country,assets)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
-function buildCountryBankCountBar(country) {
+function buildCountryBankCountLine(country) {
     // fetch data
-    let URL = '/bankcount/' + country;
+    let URL = '/bankcountrycount/' + country;
     d3.json(URL).then(function(data) {
         let dates = data[0]
         let totals = data[1]
@@ -9,17 +9,129 @@ function buildCountryBankCountBar(country) {
         var barchartdata = [{
             x: dates,
             y: totals,
-            type: "bar"
+            type: "scatter"
            }];
            var barchartlayout = {
             title: "The number of top banks in a country over time.",
             
            };
-           Plotly.newPlot("countrybankcountbar",barchartdata, barchartlayout)
+           Plotly.newPlot("countrybankcountline",barchartdata, barchartlayout)
     });
 };
+
+function buildTimeBankCountBar(timeperiod) {
+    // fetch data
+    let URL = '/banktimecount/' + timeperiod;
+    d3.json(URL).then(function(data) {
+        let countries = data[0]
+        let totals = data[1]
+        console.log('break')
+        console.log(data)
+        console.log(countries)
+        var barchartdata = [{
+            x: countries,
+            y: totals,
+            type: "bar"
+           }];
+           var barchartlayout = {
+            title: "The number of banks in each country at the selected time.",
+            
+           };
+           Plotly.newPlot("timebankcountbar",barchartdata, barchartlayout)
+    });
+};
+
+function buildCountrySumLine(country) {
+    // fetch data
+    let URL = '/tacountrysum/' + country;
+    d3.json(URL).then(function(data) {
+        let dates = data[0]
+        let totals = data[1]
+        console.log('break')
+        console.log(data)
+        console.log(dates)
+        var barchartdata = [{
+            x: dates,
+            y: totals,
+            type: "scatter"
+           }];
+           var barchartlayout = {
+            title: "The rate of change in total assets in the selected country over time.",
+            
+           };
+           Plotly.newPlot("tacountrysumline",barchartdata, barchartlayout)
+    });
+};
+
+
+function buildTimeSumBar(timeperiod) {
+    // fetch data
+    let URL = '/tatimesum/' + timeperiod;
+    d3.json(URL).then(function(data) {
+        let countries = data[0]
+        let totals = data[1]
+        console.log('break')
+        console.log(data)
+        console.log(countries)
+        var barchartdata = [{
+            x: countries,
+            y: totals,
+            type: "bar"
+           }];
+           var barchartlayout = {
+            title: "The sum of assets in each country at the selected time.",
+            
+           };
+           Plotly.newPlot("tatimesumbar",barchartdata, barchartlayout)
+    });
+};
+
+function buildBankAssetsLine(bank) {
+    // fetch data
+    let URL = '/bankassets/' + bank;
+    d3.json(URL).then(function(data) {
+        let dates = data[0]
+        let totals = data[1]
+        console.log('break')
+        console.log(data)
+        console.log(dates)
+        var barchartdata = [{
+            x: dates,
+            y: totals,
+            type: "scatter"
+           }];
+           var barchartlayout = {
+            title: "The change in assets over time at the selected bank.",
+            
+           };
+           Plotly.newPlot("bankassetsline",barchartdata, barchartlayout)
+    });
+};
+
+function buildbankstable(timeperiod) {
+    // fetch data
+    let URL = '/tables/' + timeperiod;
+    d3.json(URL).then(function(data) {
+        let banks = data[0]
+        let countries = data[1]
+        let assets = data[2]
+        console.log('break')
+        console.log(banks)
+        console.log('break')
+        console.log(countries)
+        console.log('break')
+        console.log(assets)
+    });
+};
+
+
 function init(){
 
-   buildCountryBankCountBar('Spain');
+   buildCountryBankCountLine('Spain');
+   buildTimeBankCountBar('2017-12-31 00:00:00.000000');
+   buildCountrySumLine('Spain');
+   buildTimeSumBar('2017-12-31 00:00:00.000000')
+   buildBankAssetsLine('Bank of China');
+   buildbankstable('2017-12-31 00:00:00.000000'); 
 };
 init();
